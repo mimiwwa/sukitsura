e9f035e0dec7c87d0e59ff5a6cf4b286da0c906a
mimiwwa <keiiiiiiic0@gamil.com>
2016-05-08 17:25:59 +0900

    add user seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,7 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+User.create(email: 'tw_user@testtest.com', password: 'aoyama1choume', nickname: 'Twuser')
+
+
