81cfa47c05be5de6e01cf8d0da43f1ee48c1496a
mimiwwa <keiiiiiiic0@gamil.com>
2016-05-08 18:37:07 +0900

    articlesテーブルの文字コードを変更した

--- db/migrate/20160427112814_create_articles.rb
+++ db/migrate/20160427112814_create_articles.rb
@@ -1,6 +1,6 @@
 class CreateArticles < ActiveRecord::Migration
   def change
-    create_table :articles do |t|
+    create_table :articles, options: 'ENGINE=InnoDB DEFAULT CHARSET=utf8mb4' do |t|
       t.string      :name
       t.text        :text
       t.timestamps
