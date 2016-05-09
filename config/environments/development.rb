7b124ced38eefe377d1408139b77789f9d98ab69
mimiwwa <keiiiiiiic0@gamil.com>
2016-05-09 14:57:08 +0900

    devise送信用のメールのホスト名の設定変更

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -26,7 +26,7 @@ config.action_mailer.smtp_settings = {
   :authentication => 'login',
 }
 
-config.action_mailer.default_url_options = { :host => 'localhost:3000' }
+config.action_mailer.default_url_options = { :host => 'sukitsura.herokuapp.com' }
 
 
   # Print deprecation notices to the Rails logger.
