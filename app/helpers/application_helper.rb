6a457543eb1945bae035bc976e479a2e425014f3
mimiwwa <keiiiiiiic0@gamil.com>
2016-05-12 15:51:05 +0900

    og関連のタグをテスト的に追加

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,10 +2,28 @@ module ApplicationHelper
   def default_meta_tags
   {
     site: 'すきつら',
+    reverse: true,
     separator: '-',
     title: '',
     charset: 'utf-8',
-    reverse: true
+
+    og: {
+      title: :title,
+      type: "website",
+      url: request.original_url,
+      image: 'image_url(Settings.site.meta.ogp.image_path)',
+      site_name: '好きすぎて辛い想い共有アプリ「すきつら」',
+      description: :description,
+      locale: 'ja_JP'
+    }
   }
+  end
+
+
+
 end
-end
+
+
+
+
+
